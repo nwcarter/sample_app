ce8539f2a44967b88c9ffb0b1660b90e289168fd
Nathan Carter <nc8510@mcla.edu>
2015-03-03 12:03:12 -0500

    Started work on account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,19 @@ User.create!(name:  "Nathan Carter",
              email: "nc8510@icloud.com",
              password:              "nathan",
              password_confirmation: "nathan",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
+
